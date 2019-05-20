3f07c8ca147da30e9592db9d656fea44609a7101
rachmalia <dewimassiver@gmail.com>
2019-05-20 20:52:02 +0700

    Add files via upload

--- Grafika Komputer dan Visualisasi/Grafika Komputer dan Visualisasi/vs/Lesson04/Demo.cpp
+++ Grafika Komputer dan Visualisasi/Grafika Komputer dan Visualisasi/vs/Lesson04/Demo.cpp
@@ -23,7 +23,11 @@ void Demo::Init() {
 
 	BuildColoredLocomotif();
 
+	BuildColoredTrack();
+
 	InitCamera();
+
+	initSky();
 }
 
 void Demo::DeInit() {
@@ -38,6 +42,9 @@ void Demo::DeInit() {
 	glDeleteVertexArrays(1, &VAO3);
 	glDeleteBuffers(1, &VBO3);
 	glDeleteBuffers(1, &EBO3);
+	glDeleteVertexArrays(1, &VAO4);
+	glDeleteBuffers(1, &VBO4);
+	glDeleteBuffers(1, &EBO4);
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
@@ -142,6 +149,10 @@ void Demo::Render() {
 
 	DrawColoredLocomotif();
 
+	DrawColoredTrack();
+
+	Draw_Skybox(viewCamX, viewCamY, viewCamZ, this->screenWidth, this->screenHeight, 250);
+
 	glDisable(GL_DEPTH_TEST);
 }
 
@@ -154,7 +165,7 @@ void Demo::BuildColoredCube() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	int width, height;
-	unsigned char* image = SOIL_load_image("crate.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	unsigned char* image = SOIL_load_image("grey.png", &width, &height, 0, SOIL_LOAD_RGBA);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -197,7 +208,7 @@ void Demo::BuildColoredCube() {
 		-5, -0.5, -0.5, 0, 0, // 20
 		5, -0.5, -0.5, 1, 0,  // 21
 		5, -0.5,  0.5, 1, 1,  // 22
-		-5, -0.5,  0.5, 0, 1, // 23
+		-5, -0.5,  0.5, 0, 1 // 23
 	};
 
 	unsigned int indices[] = {
@@ -206,7 +217,7 @@ void Demo::BuildColoredCube() {
 		8,  9,  10, 8,  10, 11,  // back
 		12, 14, 13, 12, 15, 14,  // left
 		16, 18, 17, 16, 19, 18,  // upper
-		20, 22, 21, 20, 23, 22,  // bottom
+		20, 22, 21, 20, 23, 22  // bottom
 	};
 
 	glGenVertexArrays(1, &VAO);
@@ -268,7 +279,7 @@ void Demo::BuildColoredPlane()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width, height;
-	unsigned char* image = SOIL_load_image("marble.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	unsigned char* image = SOIL_load_image("grass.png", &width, &height, 0, SOIL_LOAD_RGBA);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	SOIL_free_image_data(image);
@@ -281,7 +292,7 @@ void Demo::BuildColoredPlane()
 		-50.0, -0.5, -50.0,  0,  0,
 		 50.0, -0.5, -50.0, 50,  0,
 		 50.0, -0.5,  50.0, 50, 50,
-		-50.0, -0.5,  50.0,  0, 50,
+		-50.0, -0.5,  50.0,  0, 50
 	};
 
 	GLuint indices[] = { 0,  2,  1,  0,  3,  2 };
@@ -345,34 +356,34 @@ void Demo::BuildColoredLocomotif() {
 	float vertices[] = {
 		// format position, tex coords
 		// front
-		3.5, 0, 0.5, 0, 0,  // 0
-		4.6, 0, 0.5, 1, 0,   // 1
-		 4.6,  0.5, 0.5, 1, 1,   // 2
-		3.5,  0.5, 0.5, 0, 1,  // 3
+		3.5, 0.75, 0.5, 0, 0,  // 0
+		4.6, 0.75, 0.5, 1, 0,   // 1
+		4.6,  0.25, 0.5, 1, 1,   // 2
+		3.5,  0.25, 0.5, 0, 1,  // 3
 
 		// right
-		4.6,  0.5,  0.5, 0, 0,  // 4
-		4.6,  0.5, -0.5, 1, 0,  // 5
-		4.6, 0, -0.5, 1, 1,  // 6
-		4.6, 0,  0.5, 0, 1,  // 7
+		4.6,  0.75,  0.5, 0, 0,  // 4
+		4.6,  0.75, -0.5, 1, 0,  // 5
+		4.6, 0.25, -0.5, 1, 1,  // 6
+		4.6, 0.25,  0.5, 0, 1,  // 7
 
 		// back
-		3.5, -0.5, -0.5, 0, 0, // 8 
-		4.6,  -0.5, -0.5, 1, 0, // 9
-		4.6,   0.5, -0.5, 1, 1, // 10
-		3.5,  0.5, -0.5, 0, 1, // 11
+		3.5, 0.25, -0.5, 0, 0, // 8 
+		4.6,  0.25, -0.5, 1, 0, // 9
+		4.6,   0.75, -0.5, 1, 1, // 10
+		3.5,  0.75, -0.5, 0, 1, // 11
 
 		// left
-		3.5, 0.5, -0.5, 0, 0, // 12
-		3.5, 0.5,  0.5, 1, 0, // 13
-		3.5,  0,  0.5, 1, 1, // 14
-		3.5,  0, -0.5, 0, 1, // 15
+		3.5, 0.55, -0.5, 0, 0, // 12
+		3.5, 0.55,  0.5, 1, 0, // 13
+		3.5,  0.25,  0.5, 1, 1, // 14
+		3.5,  0.25, -0.5, 0, 1, // 15
 
 		// bottom
-		-0.5, 0, -0.5, 0, 0, // 16
-		0.5, 0, -0.5, 1, 0,  // 17
-		0.5, 0,  0.5, 1, 1,  // 18
-		-0.5, 0,  0.5, 0, 1, // 19
+		3.5, 0.25, -0.5, 0, 0, // 16
+		4.6, 0.25, -0.5, 1, 0,  // 17
+		4.6, 0.25,  0.5, 1, 1,  // 18
+		3.5, 0.25,  0.5, 0, 1 // 19
 	};
 
 	unsigned int indices[] = {
@@ -380,7 +391,7 @@ void Demo::BuildColoredLocomotif() {
 		4,  5,  6,  4,  6,  7,   // right
 		8,  9,  10, 8,  10, 11,  // back
 		12, 14, 13, 12, 15, 14,  // left
-		16, 18, 17, 16, 19, 18,  // bottom
+		16, 18, 17, 16, 19, 18  // bottom
 	};
 
 	glGenVertexArrays(1, &VAO3);
@@ -442,6 +453,84 @@ void Demo::DrawColoredLocomotif()
 	glBindVertexArray(0);
 }
 
+void Demo::BuildColoredTrack() {
+	// load image into texture memory
+	// ------------------------------
+	// Load and create a texture 
+	glGenTextures(1, &texture4);
+	glBindTexture(GL_TEXTURE_2D, texture4);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	int width, height;
+	unsigned char* image = SOIL_load_image("track.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float vertices[] = {
+		// format position, tex coords
+		// bottom
+		-5, 0.01, -0.5, 0, 0, // 0
+		5, 0.01, -0.5, 1, 0,  // 1
+		5, 0.01,  0.5, 1, 1,  // 2
+		-5, 0.01,  0.5, 0, 1 // 3
+	};
+
+	unsigned int indices[] = {
+		0,  2,  1,  0,  3,  2   // bottom
+	};
+
+	glGenVertexArrays(1, &VAO4);
+	glGenBuffers(1, &VBO4);
+	glGenBuffers(1, &EBO4);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO4);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO4);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO4);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// define position pointer layout 0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(0);
+
+	// define texcoord pointer layout 1
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+}
+
+void Demo::DrawColoredTrack()
+{
+	//render
+	glUseProgram(shaderProgram);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture4);
+	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 0);
+
+	glBindVertexArray(VAO4); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindVertexArray(0);
+}
+
 void Demo::InitCamera()
 {
 	posCamX = 0.0f;
@@ -458,7 +547,6 @@ void Demo::InitCamera()
 	glfwSetInputMode(this->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 }
 
-
 void Demo::MoveCamera(float speed)
 {
 	float x = viewCamX - posCamX;
@@ -469,8 +557,6 @@ void Demo::MoveCamera(float speed)
 	viewCamX = viewCamX + x * speed;
 	viewCamZ = viewCamZ + z * speed;
 }
-
-
 
 void Demo::StrafeCamera(float speed)
 {
@@ -494,9 +580,112 @@ void Demo::RotateCamera(float speed)
 	viewCamX = (float)(posCamX + glm::cos(speed) * x - glm::sin(speed) * z);
 }
 
+void Demo::Draw_Skybox(float x, float y, float z, float width, float height, float length)
+{
+	glDisable(GL_DEPTH_TEST);
+	x = x - width / 2;
+	y = y - height / 2;
+	z = z - length / 2;
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, skybox[SKY_UP]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(x + width, y + height, z);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(x + width, y + height, z + length);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(x, y + height, z + length);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(x, y + height, z);
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D, skybox[SKY_FRONT]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(x + width, y, z);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(x + width, y, z + length);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(x + width, y + height, z + length);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(x + width, y + height, z);
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D, skybox[SKY_BACK]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(x, y + height, z);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(x, y + height, z + length);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(x, y, z + length);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(x, y, z);
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D, skybox[SKY_RIGHT]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(x + width, y, z);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(x + width, y + height, z);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(x, y + height, z);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(x, y, z);
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D, skybox[SKY_LEFT]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(x, y, z + length);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(x, y + height, z + length);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(x + width, y + height, z + length);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(x + width, y, z + length);
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D, skybox[SKY_DOWN]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(x + width, y, z + length);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(x + width, y, z);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(x, y, z);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(x, y, z + length);
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
+}
+
+void Demo::initSky()
+{
+	//Langit
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	
+	skybox[SKY_DOWN] = LoadBMP("down.bmp");
+	skybox[SKY_FRONT] = LoadBMP("front.bmp");
+	skybox[SKY_BACK] = LoadBMP("back.bmp");
+	skybox[SKY_RIGHT] = LoadBMP("right.bmp");
+	skybox[SKY_LEFT] = LoadBMP("left.bmp");
+	skybox[SKY_UP] = LoadBMP("up.bmp");
+	
+}
+
+GLuint Demo::LoadBMP(const char *fileName)
+{
+	FILE* file;
+	unsigned char header[54], * data;
+	unsigned int dataPos, size, width, height;
+	file = fopen(fileName, "rb");
+	fread(header, 1, 54, file);				//Windows BMP begin with 54 byte header
+	dataPos = *(int*) & (header[0x0A]);	//dec10, Actual BMP data
+	size = *(int*) & (header[0x22]);	//dec34, BMP Size
+	width = *(int*) & (header[0x12]);	//dec18, Image Width
+	height = *(int*) & (header[0x16]);	//dec22, Image Height
+	if (size == NULL)
+		size = width * height * 3;
+	if (dataPos == NULL)
+		dataPos = 54;
+	data = new unsigned char[size];
+	fread(data, 1, size, file);
+	fclose(file);
+	GLuint texture;
+	glGenTextures(1, &texture);				//Generate (allocate) 1 texture name
+	glBindTexture(GL_TEXTURE_2D, texture);	//Bind the 2D texture
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);	//MAG filter
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);	//MIN filter
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, 0x80E0, GL_UNSIGNED_BYTE, data); //target, level, internalFormat, width, height,border, format, type, data 
+	return texture;
+}
+
 
 int main(int argc, char** argv) {
 	RenderEngine &app = Demo();
 	app.Start("Grafika Komputer: Roller Coaster", 800, 600, false, true);
-	
 }
--- Grafika Komputer dan Visualisasi/Grafika Komputer dan Visualisasi/vs/Lesson04/Demo.h
+++ Grafika Komputer dan Visualisasi/Grafika Komputer dan Visualisasi/vs/Lesson04/Demo.h
@@ -1,4 +1,5 @@
 #pragma once
+#define GL_BGR_EXT
 #include "RenderEngine.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -13,9 +14,11 @@ public:
 	Demo();
 	~Demo();
 private:
-	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2, VBO3, VAO3, EBO3, texture3;
+	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2, VBO3, VAO3, EBO3, texture3, VBO4, VAO4, EBO4, texture4;
 	float viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
 	float angle = 0;
+	GLint skybox[6];
+	const int SKY_FRONT = 0, SKY_RIGHT = 1, SKY_LEFT = 2, SKY_BACK = 3, SKY_UP = 4, SKY_DOWN = 5;
 	virtual void Init();
 	virtual void DeInit();
 	virtual void Update(double deltaTime);
@@ -24,12 +27,17 @@ private:
 	void BuildColoredCube();
 	void BuildColoredPlane();
 	void BuildColoredLocomotif();
+	void BuildColoredTrack();
 	void DrawColoredCube();
 	void DrawColoredPlane();
 	void DrawColoredLocomotif();
+	void DrawColoredTrack();
 	void MoveCamera(float speed);
 	void StrafeCamera(float speed);
 	void RotateCamera(float speed);
 	void InitCamera();
+	void Draw_Skybox(float x, float y, float z, float width, float height, float length);
+	void initSky();
+	GLuint LoadBMP(const char *fileName);
 };
 
